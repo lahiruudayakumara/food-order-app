6cd32b9d3f66c6446a2b1d36f68f0bae023812fe
lahiruudayakumara <lahiruudayakumara950@gmail.com>
2023-12-09 02:42:01 +0530

    add user controller

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
-const createUser = async (req, res) => {
+//SignUp User
+const signup = async (req, res) => {
     try {
-        const {
-            name,
-            email,
-            _id,
-        } = req.body;
+        const { name, email, password } = req.body;
 
         const isEmail = await User.findOne({ email: email})
 
@@ -15,20 +14,40 @@ const createUser = async (req, res) => {
             return res.status(200).json({ msg: "user already exists"})
         }
     
-        const user = new User({
-            name,
-            email,
-            _id,
-        })
+        const user = await new User({ name, email, password })
 
         user.save()
-        return res.status(200).json({ data: user});
+        return res.status(200).json({email, token});
         
     } catch (err) {
         return res.status(400).json({ data : err });
     }
 }
 
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if(!user) {
+            return res.status(400).json({ msg: "User does not exists"});
+        }
+
+        const isMatch = await User.findOne({ password: password });
+        if(!isMatch) {
+            return res.status(400).json({ msg : "Invalid Creditional"});
+        }
+
+        const userCopy = {...user.toObject()}
+        delete userCopy.password;
+        const token = jwt.sign(userCopy, "sadsdasdasd");
+
+        return res.status(200).json({ token, user });
+    } catch(err) {
+        return res.status(400).json({ data : err });
+    }
+}
+
 module.exports = {
-    createUser
+    signup,
+    login
 }
