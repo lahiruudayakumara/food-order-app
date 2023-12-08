d2afa029dfe1dc87b65ccd9ba6f8471185f7bfed
lahiruudayakumara <lahiruudayakumara950@gmail.com>
2023-12-09 02:40:17 +0530

    add server index

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const DBConnection = require('./config/dbConfig');
+const product = require('./routes/productRoutes');
+const user = require("./routes/userRoutes")
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+
+const PORT = process.env.PORT || 8080;
+
+// Routes
+app.use('/product', product);
+app.use('/user', user);
+
+DBConnection().then(() => {
+    app.listen(PORT, () => {
+        console.log('🚀 Database Connect Sucessfully 📚');
+        console.log(`🚀🚀🚀 Server runing Port is ${PORT}🚀`);
+    });
+}).catch((err) => {
+    console.error('While connecting with the database', err.message);
+});
+
