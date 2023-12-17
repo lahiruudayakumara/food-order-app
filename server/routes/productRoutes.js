5b7154ad654a52ad88e13e6e042575716861a41e
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-12-17 21:22:34 +0530

    add verify token

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getProducts, getCategoryProducts } = require('../controller/productController');
+const { verifyToken } = require('../middleware/auth');
 
-router.get('/list', getProducts);
-router.get('/list-category', getCategoryProducts);
+router.get('/list', verifyToken, getProducts);
+router.get('/list-category', getCategoryProducts, verifyToken);
 
 module.exports = router;
