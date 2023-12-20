2fcaa1e36a5f963149e3f3dc2760676ed42988ac
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-12-20 23:21:44 +0530

    cart slice improvement

--- client/src/stores/cart/cartSlice.js
+++ client/src/stores/cart/cartSlice.js
@@ -9,23 +9,28 @@ export const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            return { products: [...state.products, {...action.payload, amount: 1}]}
+            return { products: [...state.products, {...action.payload, amount: 1, totalPrice: parseInt(action.payload.price)}]}
         },
         clearCart: (state) => {
             return { products: []}
         },
+        removeItem: (state, action) => {
+            return { products: state.products.filter((product) => product._id !== action.payload._id) };
+        },
         incrementProductAmount: (state, action) => {
             console.log('increment');
-            return { products: state.products.map(product => product.id === action.payload.id ? {...product, amount: product.amount + 1} : product)}
+            return { 
+                products: state.products.map(product => product._id === action.payload._id ? {...product, amount: product.amount + 1, totalPrice: product.totalPrice + parseInt(action.payload.price, 10)} : product)
+            }
         },
         decrementProductAmount: (state, action) => {
-            return { products: state.products.map(product => product.id === action.payload.id ? {...product, amount: product.amount - 1} : product)}
+            return { products: state.products.map(product => product._id === action.payload._id ? {...product, amount: product.amount - 1, totalPrice: product.totalPrice - parseInt(product.price, 10)} : product)}
         }
     }
 })
 
 export const cartProducts = state => state.cart.products
 
-export const {  addToCart, clearCart, incrementProductAmount, decrementProductAmount } = cartSlice.actions
+export const {  addToCart, clearCart, removeItem, incrementProductAmount, decrementProductAmount } = cartSlice.actions
 
 export default cartSlice.reducer
