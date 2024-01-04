8dee9b5e5cc1f60c37cdddb0f2d6beeaf069501d
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-01-04 23:45:03 +0530

    ui improve

--- client/src/components/ProductPreviewCard.jsx
+++ client/src/components/ProductPreviewCard.jsx
@@ -1,19 +1,28 @@
 import AddProduct from './AddProduct'
+import Rating from '@mui/material/Rating';
 
 const ProductPreviewCard = ({ product, onAddProduct }) => {
 
     const addProduct = () => {
       onAddProduct(product)
     }
+
+    const sumRating = product.rating.reduce((sum, rating) => sum + rating, 0)
+    const averageRating = sumRating / product.rating.length
     
   return (
-    <div className='w-full p-4 m-2 rounded-2xl text-white shadow-sm bg-slate-700 text-center'>
-      <img src={product.imgUrl} alt={product.name} />
-      <h2 className='text-lg'>{product.name}</h2>
-      <h6 className='pb-1 text-sm'>{product.adjective}</h6>
-      <p className='mb-1 text-xs h-24 line-clamp-4 justify-normal text-justify'>{product.description}</p>
-      <p className='line-clamp-4 justify-normal'>${product.price}</p>
-      <AddProduct onAddProduct={addProduct} />
+    <div className='w-full p-4 m-2 rounded-md text-white shadow-sm bg-[#F2F2F2] text-center'>
+      <img src={product.imgUrl} alt={product.name} className='' />
+      <h2 className='text-lg font-bold text-left text-black'>{product.name}</h2>
+      <p className='mb-1 text-xs h-24 line-clamp-4 justify-normal text-justify text-[#525252]'>{product.description}</p>
+      <div className='flex justify-between'>
+        <p className='line-clamp-4 justify-normal font-bold text-left text-black'>${product.price}</p>
+        <AddProduct onAddProduct={addProduct} />
+      </div>
+      <div className={`flex items-center justify-center ${averageRating? 'text-[#f1c200]' : 'text-[#525252]'}`}>
+        <span>{averageRating? averageRating : 0 }</span>
+        <Rating name="read-only" defaultValue={averageRating} precision={0.5} readOnly/>
+      </div>
     </div>
   )
 }
