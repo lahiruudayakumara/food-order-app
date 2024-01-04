2de103848275b8492126569e02925e606b5423c9
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-01-04 23:37:43 +0530

    ui improve

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -1,7 +1,7 @@
 const AddProduct = ({ onAddProduct }) => {
   return (
     <div className='flex justify-end'>
-        <button onClick={onAddProduct} className='bg-yellow-400 hover:bg-yellow-500 rounded-full w-6 h-6  flex items-center justify-center font-bold text-xl'><span>+</span></button>      
+        <button onClick={onAddProduct} className='bg-yellow-400 hover:bg-yellow-500 rounded-md h-6 px-2  flex items-center justify-center font-bold text-xs'><span>Add to Cart</span></button>      
     </div>
   )
 }
